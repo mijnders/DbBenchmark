bb3f00b2e6c87fea2945a93b343d25c8dbcb61ee
van-kempen <til-philip.van-kempen@adesso.de>
2023-06-07 10:40:40 +0200

    added sql statements for data import and postgres driver

--- src/PostgresConnector.java
+++ src/PostgresConnector.java
@@ -11,34 +11,45 @@ public class PostgresConnector {
     public static String password = "geheim";
 
     //Data import
-    public static String createTables = "create ...";
-    public static String csvLocation = "C:\\Berufsschule\\Blockwoche\\DbBenchmark\\persons.csv";
-    public static String importDataQuery = "COPY persons(first_name, last_name, dob, email)\n" +
-            "            FROM '" + csvLocation + "'\n" +
-            "            DELIMITER ','\n" +
-            "            CSV HEADER;";
+    public static String createTables =
+                    "create table Product (id INT PRIMARY KEY, Name TEXT, Price FLOAT);" +
+                    "create table Customer (id INT PRIMARY KEY, Firstname TEXT, Lastname TEXT, Street TEXT, City TEXT);" +
+                    "create table Invoice (id INT PRIMARY KEY, CustomerID INT, Total FLOAT, CONSTRAINT customer_fk FOREIGN KEY(CustomerID) REFERENCES Customer(id));"+
+                    "create table Item (InvoiceID INT, Item INT, ProductID INT, Quantity INT, Cost FLOAT, PRIMARY KEY(InvoiceID, Item), " +
+                            "CONSTRAINT invoice_fk FOREIGN KEY(InvoiceID) REFERENCES Invoice(id), CONSTRAINT product_fk FOREIGN KEY(ProductID) REFERENCES Product(id));";
 
-    //
-    public static String CreateStatement = "";
-    public static String ReadStatement = "";
-    public static String UpdateStatement = "";
-    public static String DeleteStatement = "";
+    private Connection connection;
 
-    public void importData() throws SQLException, ClassNotFoundException{
-        Statement statement = getStatement(getConnection());
-        statement.execute(createTables);
-        statement.execute(importDataQuery);
+    public PostgresConnector() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=geheim",
+                    "postgres",
+                    "geheim");
+        }
+        catch (Exception e) {
+            System.err.println("Connection failed!");
+            e.printStackTrace();
+        }
     }
 
-    public Connection getConnection() throws SQLException, ClassNotFoundException {
-        Class.forName("org.postgresql.Driver");
-        return DriverManager.getConnection(
-                "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=geheim",
-                "postgres",
-                "geheim");
+    public void sendStatement(String s) throws SQLException, ClassNotFoundException{
+        Statement statement = connection.createStatement();
+        statement.execute(s);
     }
 
-    public Statement getStatement(Connection connection) throws SQLException{
-        return connection.createStatement();
+    public void importDataFromCSV(String productPath, String customerPath, String invoicePath, String itemPath){
+        String importQuery =
+                        "COPY Customer(id, Firstname, Lastname, Street, City) FROM '" + customerPath + "'DELIMITER ',' CSV HEADER;";
+        try {
+            sendStatement(importQuery);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
+
+// +
+//                        "COPY Product(id, Name, Price) FROM '" + productPath + "' DELIMITER ',' CSV HEADER;";
