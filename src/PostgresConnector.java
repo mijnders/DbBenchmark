6877c46526bb119d52dda197ed56d71eb14e01da
van-kempen <til-philip.van-kempen@adesso.de>
2023-06-07 13:52:27 +0200

    cleaned strings

--- src/PostgresConnector.java
+++ src/PostgresConnector.java
@@ -3,17 +3,9 @@ import java.sql.*;
 public class PostgresConnector implements IDatabaseConnector{
 
     //Connection string
-    public static String postgresURL = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=geheim";
-    public static String user = "postgres";
-    public static String password = "geheim";
-
-    //Data import
-    public static String createTables =
-                    "create table Product (id INT PRIMARY KEY, Name TEXT, Price FLOAT);" +
-                    "create table Customer (id INT PRIMARY KEY, Firstname TEXT, Lastname TEXT, Street TEXT, City TEXT);" +
-                    "create table Invoice (id INT PRIMARY KEY, CustomerID INT, Total FLOAT, CONSTRAINT customer_fk FOREIGN KEY(CustomerID) REFERENCES Customer(id));"+
-                    "create table Item (InvoiceID INT, Item INT, ProductID INT, Quantity INT, Cost FLOAT, PRIMARY KEY(InvoiceID, Item), " +
-                            "CONSTRAINT invoice_fk FOREIGN KEY(InvoiceID) REFERENCES Invoice(id), CONSTRAINT product_fk FOREIGN KEY(ProductID) REFERENCES Product(id));";
+    private static final String postgresURL = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=geheim";
+    private static final String user = "postgres";
+    private static final String password = "geheim";
 
     private Connection connection;
 
@@ -22,9 +14,9 @@ public class PostgresConnector implements IDatabaseConnector{
         try {
             Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=geheim",
-                    "postgres",
-                    "geheim");
+                    postgresURL,
+                    user,
+                    password);
         }
         catch (Exception e) {
             System.err.println("Connection failed!");
@@ -63,12 +55,19 @@ public class PostgresConnector implements IDatabaseConnector{
 
     @Override
     public boolean initializeDatabase(String... params) {
+        String createTables =
+                "create table Product (id INT PRIMARY KEY, Name TEXT, Price FLOAT);" +
+                        "create table Customer (id INT PRIMARY KEY, Firstname TEXT, Lastname TEXT, Street TEXT, City TEXT);" +
+                        "create table Invoice (id INT PRIMARY KEY, CustomerID INT, Total FLOAT, CONSTRAINT customer_fk FOREIGN KEY(CustomerID) REFERENCES Customer(id));"+
+                        "create table Item (InvoiceID INT, Item INT, ProductID INT, Quantity INT, Cost FLOAT, PRIMARY KEY(InvoiceID, Item), " +
+                        "CONSTRAINT invoice_fk FOREIGN KEY(InvoiceID) REFERENCES Invoice(id), CONSTRAINT product_fk FOREIGN KEY(ProductID) REFERENCES Product(id));";
+
         String importQuery =
                 "COPY Customer(id, Firstname, Lastname, Street, City) FROM '" + params[0] + "'DELIMITER ',' CSV HEADER;" +
                 "COPY Product(id, Name, Price) FROM '" + params[1] + "' DELIMITER ',' CSV HEADER;" +
                 "COPY Invoice(id, CustomerID, Total) '" + params[2] + "' DELIMITER ',' CSV HEADER;" +
                 "COPY Item(InvoiceID, Item, ProductID, Quantity, Cost) '" + params[3] + "' DELIMITER ',' CSV HEADER;";
 
-        return send(importQuery);
+        return send(createTables + importQuery);
     }
 }
