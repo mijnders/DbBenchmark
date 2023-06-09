fbf76796faa692c2ad14bf5208153031fda93596
Niklas Mijnders <niklas.mijnders@adesso.de>
2023-06-09 13:46:09 +0200

    add test folder and create test file

---
+++ test/Neo4JCreateTest.java
@@ -0,0 +1,26 @@
+class Neo4JCreateTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createOne() {
+    }
+    @org.junit.jupiter.api.Test
+    void createOneHundred() {
+    }
+    @org.junit.jupiter.api.Test
+    void createTenThousand() {
+    }
+    @org.junit.jupiter.api.Test
+    void createHundredThousand() {
+    }
+    @org.junit.jupiter.api.Test
+    void createOneMillion() {
+    }
+}
